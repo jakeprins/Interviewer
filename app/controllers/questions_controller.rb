8eaa146d24f09a591ff4b4c605b13183e34ea71e
jakeprins <jakeprins@outlook.com>
2016-05-01 14:51:24 +0200

    create option

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,6 @@
 class QuestionsController < ApplicationController
   before_filter :set_category
-  
+
   def index
     @questions = @category.questions
   end
@@ -9,7 +9,20 @@ class QuestionsController < ApplicationController
     @question = @category.questions.find(params[:id])
   end
 
+  def new
+    @question = Question.new
+    @question.category_id = params[:category_id]
+  end
+
   def create
+    question = Question.new( question_params )
+
+      if question.save
+        flash[:notice] = "Thanks for adding a new question!"
+        redirect_to category_path(@category)
+      else
+        render :new
+      end
   end
 
   protected
@@ -17,4 +30,9 @@ class QuestionsController < ApplicationController
   def set_category
     @category = Category.find(params[:category_id])
   end
+
+  def question_params
+    params.require( :question ).permit( :question, :answer)
+  end
+
 end
