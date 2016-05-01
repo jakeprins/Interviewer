eb349c67872bb1ef8d9e62297e882feb4cf2f3da
jakeprins <jakeprins@outlook.com>
2016-05-01 21:24:08 +0200

    Working create

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,34 +5,30 @@ class QuestionsController < ApplicationController
     @questions = @category.questions
   end
 
-  def show
-    @question = @category.questions.find(params[:id])
-  end
-
   def new
     @question = Question.new
     @question.category_id = params[:category_id]
   end
 
   def create
-    question = Question.new( question_params )
+    @question = Question.create!( question_params )
+    @question.category = @category
 
-      if question.save
+      if @question.save
         flash[:notice] = "Thanks for adding a new question!"
-        redirect_to category_path(@category)
+        redirect_to category_questions_path(@category)
       else
         render :new
       end
-  end
-
-  protected
-
-  def set_category
-    @category = Category.find(params[:category_id])
   end
 
   def question_params
     params.require( :question ).permit( :question, :answer)
   end
 
+  private
+
+  def set_category
+    @category = Category.find(params[:category_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   resources :categories do
     resources :questions
   end
+
 end
