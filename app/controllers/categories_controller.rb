4c0c5ccced4d0c94a33cac22145143ce3277e1df
jakeprins <jakeprins@outlook.com>
2016-05-09 15:19:04 +0200

    Migrated descriptions to subjects

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -22,7 +22,7 @@ class CategoriesController < ApplicationController
   end
 
   def category_params
-    params.require( :category ).permit( :name)
+    params.require( :category ).permit( :name, :description)
   end
 
 end
