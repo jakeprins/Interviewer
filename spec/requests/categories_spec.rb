d66937282eba0eccda1087d9482e819d3790f965
jakeprins <jakeprins@outlook.com>
2016-05-12 11:20:27 +0200

    Updated specs CategoriesController + small clean up

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Categories", type: :request do
-  let(:category) { Category.create(name: "CSS", description: "Styling stuff") }
-
+  
   describe "GET /categories" do
     it "works" do
       get categories_path
